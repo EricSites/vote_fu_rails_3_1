3c42d78e2cf42ccb4a8a5f899cf9dffae8ebfa13
EricSites <eric_sites@mac.com>
2012-04-10 19:39:47 -0300

    Change vote type to integer

--- lib/acts_as_voteable.rb
+++ lib/acts_as_voteable.rb
@@ -89,14 +89,14 @@ module Juixe
         def votes_for
           Vote.count(:all, :conditions => [
             "voteable_id = ? AND voteable_type = ? AND vote = ?",
-            id, self.class.name, true
+            id, self.class.name, 1
           ])
         end
 
         def votes_against
           Vote.count(:all, :conditions => [
             "voteable_id = ? AND voteable_type = ? AND vote = ?",
-            id, self.class.name, false
+            id, self.class.name, -1
           ])
         end
 
